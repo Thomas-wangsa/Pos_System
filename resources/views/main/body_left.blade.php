9726c316faaf23a2a24c0cb6d449780945094975
thomas.wangsa@finaccel.co <thomas.wangsa@finaccel.co>
2019-08-22 01:41:54 +0700

    upd

--- resources/views/main/body_left.blade.php
+++ resources/views/main/body_left.blade.php
@@ -1,10 +1,10 @@
 <div class="col-sm-3 sidenav" style="padding-top: 15px">
   <h4>John's Blog</h4>
   <ul class="nav nav-pills nav-stacked">
-    <li class="active"><a href="#section1">Home</a></li>
-    <li><a href="#section2">Friends</a></li>
-    <li><a href="#section3">Family</a></li>
-    <li><a href="#section3">Photos</a></li>
+    <li class="active"><a href="{{route('home')}}">Home</a></li>
+    <li><a href="{{route('user.index')}}">Users</a></li>
+    <li><a href="{{route('customer.index')}}">Customers</a></li>
+    <li><a href="#section3">Data</a></li>
   </ul><br>
   <div class="input-group">
     <input type="text" class="form-control" placeholder="Search Blog..">
--- resources/views/main/header.blade.php
+++ resources/views/main/header.blade.php
@@ -12,10 +12,60 @@
         </div>
         <div class="collapse navbar-collapse" id="myNavbar">
           <ul class="nav navbar-nav">
-            <li class="active"><a href="#">Home</a></li>
-            <li><a href="#">Page 1</a></li>
-            <li><a href="#">Page 2</a></li> 
-            <li><a href="#">Page 3</a></li> 
+            
+            <li class="active">
+              <a href="{{route('home')}}">
+                Home
+              </a>
+            </li>
+            
+            <li>
+              <a href="{{route('user.index')}}">
+                User
+              </a>
+            </li>
+
+            <li>
+              <a href="{{route('customer.index')}}">
+                Customer
+              </a>
+            </li>
+
+            <li>
+              <a href="#" class="dropdown-toggle" 
+              data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true" v-pre>
+                <span class="glyphicon glyphicon-user"></span> 
+                Data
+                <span class="caret"></span>
+              </a>
+
+              <ul class="dropdown-menu">
+                <li>
+                  <a href="{{ route('po.index') }}">
+                    <span class="glyphicon glyphicon-envelope"></span>
+                    PO
+                  </a>
+                </li>
+                <li>
+                  <a href="{{ route('do.index') }}">
+                    <span class="glyphicon glyphicon-edit"></span>
+                    Delivery Order
+                  </a>
+                </li>
+
+                <li>
+                  <a href="{{ route('invoice.index') }}">
+                    <span class="glyphicon glyphicon-edit"></span>
+                    Invoice
+                  </a>
+                </li>
+              </ul>
+            </li>
+
+
+
+
+            <li><a href="#"> Report </a></li> 
           </ul>
           <ul class="nav navbar-nav navbar-right">
             
