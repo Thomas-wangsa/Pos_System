94a4734ec53df32127240491169c6affc86197a3
thomas.wangsa@finaccel.co <thomas.wangsa@finaccel.co>
2019-10-12 22:51:44 +0700

    upd

--- database/migrations/2019_08_06_182803_create_table_delivery_order.php
+++ database/migrations/2019_08_06_182803_create_table_delivery_order.php
@@ -15,17 +15,47 @@ class CreateTableDeliveryOrder extends Migration
     {
         Schema::create('delivery_order', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('number')->unique();
             $table->unsignedInteger('po_id');
+            $table->unsignedInteger('customer_id');
+            $table->unsignedInteger('sales_id');
             $table->unsignedInteger('driver_id');
-            $table->string('number');
+            $table->unsignedInteger('created_by');
+            $table->unsignedInteger('updated_by');
             $table->unsignedInteger('status')->default(1);
             $table->string('uuid',100)->unique(); 
             $table->text('note')->nullable();
-            $table->date('date')->nullable();
-            $table->unsignedInteger('created_by');
-            $table->unsignedInteger('updated_by');
             $table->timestamps();
             $table->softDeletes();
+
+            $table->foreign('po_id', 'delivery_order_po_id_byfkey')
+                ->references('id')->on('po')
+                ->onUpdate('CASCADE')->onDelete('RESTRICT');
+
+            $table->foreign('customer_id', 'delivery_order_customer_id_byfkey')
+                ->references('id')->on('customer')
+                ->onUpdate('CASCADE')->onDelete('RESTRICT');
+
+            $table->foreign('sales_id', 'delivery_order_sales_id_byfkey')
+                ->references('id')->on('users')
+                ->onUpdate('CASCADE')->onDelete('RESTRICT');
+
+            $table->foreign('driver_id', 'delivery_order_driver_id_byfkey')
+                ->references('id')->on('driver')
+                ->onUpdate('CASCADE')->onDelete('RESTRICT');
+
+            $table->foreign('created_by', 'delivery_order_created_by_byfkey')
+                ->references('id')->on('users')
+                ->onUpdate('CASCADE')->onDelete('RESTRICT');
+
+
+            $table->foreign('updated_by', 'delivery_order_updated_by_byfkey')
+                ->references('id')->on('users')
+                ->onUpdate('CASCADE')->onDelete('RESTRICT');
+
+            $table->foreign('status', 'delivery_order_status_byfkey')
+                ->references('id')->on('delivery_order_status')
+                ->onUpdate('CASCADE')->onDelete('RESTRICT');
         });
     }
 
