8a60db5d496be6cfb84d3e82b6343f1c1a071231
thomas.wangsa@finaccel.co <thomas.wangsa@finaccel.co>
2019-10-25 21:36:19 +0700

    upd

--- database/migrations/2019_08_21_180005_create_sub_invoice_table.php
+++ database/migrations/2019_08_21_180005_create_sub_invoice_table.php
@@ -15,18 +15,29 @@ class CreateSubInvoiceTable extends Migration
     {
         Schema::create('sub_invoice', function (Blueprint $table) {
             $table->increments('id');
-            $table->unsignedInteger('invoice_id');
             $table->unsignedInteger('quantity');
             $table->string('name');
             $table->unsignedInteger('price');
-            $table->unsignedInteger('total')->default(0);
-            $table->unsignedInteger('status')->default(1);
-            $table->string('uuid',100)->unique(); 
+            $table->unsignedInteger('invoice_id');
             $table->text('note')->nullable();
             $table->unsignedInteger('created_by');
             $table->unsignedInteger('updated_by');
+            $table->string('uuid',100)->unique();
             $table->timestamps();
             $table->softDeletes();
+
+            $table->foreign('invoice_id', 'sub_invoice_id_byfkey')
+                ->references('id')->on('delivery_order')
+                ->onUpdate('CASCADE')->onDelete('RESTRICT');
+
+            $table->foreign('created_by', 'sub_invoice_created_by_byfkey')
+                ->references('id')->on('users')
+                ->onUpdate('CASCADE')->onDelete('RESTRICT');
+
+            $table->foreign('updated_by', 'sub_invoice_updated_by_byfkey')
+                ->references('id')->on('users')
+                ->onUpdate('CASCADE')->onDelete('RESTRICT');
+
         });
     }
 
